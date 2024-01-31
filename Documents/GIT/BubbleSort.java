d848be34bfc307d2491310dd2af9e3be2db3cc9f
Arthi807 <artgang75@gmail.com>
2024-01-31 14:49:28 -0800

    Adding Java program1

---
+++ Documents/GIT/BubbleSort.java
@@ -0,0 +1,53 @@
+package Bubble_sort;
+
+public class BubbleSort {
+	
+	 public static void bubbleSort(int[] arr) {
+	        int n = arr.length;
+	        boolean swapped;
+
+	        for (int i = 0; i < n - 1; i++) {
+	            swapped = false;
+
+	            for (int j = 0; j < n - i - 1; j++) {
+	                if (arr[j] > arr[j + 1]) {
+	                    // Swap arr[j] and arr[j + 1]
+	                    int temp = arr[j];
+	                    arr[j] = arr[j + 1];
+	                    arr[j + 1] = temp;
+	                    swapped = true;
+	                }
+	            }
+
+	            // If no two elements were swapped in inner loop, the array is already sorted
+	            if (!swapped) {
+	                break;
+	            }
+	        }
+	    }
+	
+	
+	
+	public static void main(String[] args) {
+     int[] myArray = {5, 2, 9, 1, 5};
+     
+     // Call the bubbleSort method to sort the array
+     bubbleSort(myArray);
+     
+     // Print the sorted array
+     for (int element : myArray) {
+         System.out.print(element + " ");
+     }
+ }
+	
+
+
+}
+
+	
+
+
+	
+
+
+
